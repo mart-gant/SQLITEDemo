1703a70c2faadc50187fecb634eab9d3fdaaa307
mart-gant <martgant@gmail.com>
2024-05-14 12:25:10 +0200

    Dodaj pliki projektów.

---
+++ SQLITEDemo/MVVM/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using SQLITEDemo.MVVM.ViewModels;
+
+namespace SQLITEDemo.MVVM.Views;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = new MainPageViewModel();
+    }
+}
